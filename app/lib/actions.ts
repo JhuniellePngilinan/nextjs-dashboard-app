d5731b2820de24aaaa4c2764c6b5100d632cebbc
Jhunielle Pangilinan <jl.pangilinan@servoitsolutions.ph>
2025-06-24 08:02:40 +0800

    resolved build error

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -61,7 +61,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
       VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
     `;
   } catch (error) {
-    // If a database error occurs, return a more specific error.
+    console.error("Database Error:", error);
     return {
       message: "Database Error: Failed to Create Invoice.",
     };
@@ -95,8 +95,6 @@ export async function updateInvoice(id: string, formData: FormData) {
 }
 
 export async function deleteInvoice(id: string) {
-  throw new Error("Failed to Delete Invoice");
-
   await sql`DELETE FROM invoices WHERE id = ${id}`;
   revalidatePath("/dashboard/invoices");
 }
